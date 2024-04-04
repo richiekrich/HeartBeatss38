f5127dcda5f9622a38ce1268a73a8ce04677a9fc
ediaz029 <ediazj03@gmail.com>
2024-04-04 15:59:04 -0500

    Initial Commit

---
+++ HeartBeats38/HeartBeats38App.swift
@@ -0,0 +1,32 @@
+//
+//  HeartBeats38App.swift
+//  HeartBeats38
+//
+//  Created by Ernesto Diaz on 4/4/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HeartBeats38App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ HeartBeats38/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HeartBeats38
+//
+//  Created by Ernesto Diaz on 4/4/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
