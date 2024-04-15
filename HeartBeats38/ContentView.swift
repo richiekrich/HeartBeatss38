9c5e13515674f331ffafa7276f28d7f550ad4b7e
ediaz029 <ediazj03@gmail.com>
2024-04-15 07:00:46 -0500

    added launch screen + ui changes

--- HeartBeats38/ContentView.swift
+++ HeartBeats38/ContentView.swift
@@ -3,60 +3,81 @@ import AVFoundation
 
 struct ContentView: View {
     @StateObject private var viewModel = HeartRateViewModel()
-    @StateObject private var audioPlayerManager = AudioPlayerManager(audioFileName: "Heart", fileType: "mp3")  // Correctly initialized
+    @StateObject private var audioPlayerManager = AudioPlayerManager()
     @State private var statusMessage = ""
+    @State private var isWorkoutActive = false
+    @State private var isAudioPlaying = false
 
     var body: some View {
-        VStack {
-            Text("Heart Rate: \(viewModel.heartRate, specifier: "%.1f") BPM")
-            Text(statusMessage)
-                .foregroundColor(.gray)
-                .padding()
-
-            Button("Start Workout") {
-                viewModel.startHeartRateSimulation()
-                statusMessage = "Workout started."
-                audioPlayerManager.play()  // Start playing when workout starts
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            VStack {
+                Text("Heart Rate: \(viewModel.heartRate, specifier: "%.1f") BPM").foregroundColor(.white)
+                Text(statusMessage)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Button("Start Workout") {
+                    viewModel.startHeartRateSimulation()
+                    statusMessage = "Workout started."
+                    audioPlayerManager.play()
+                    isWorkoutActive = true
+                }
+                .disabled(viewModel.isWorkoutActive)
+                .buttonStyle(PrimaryButtonStyle(isDisabled: viewModel.isWorkoutActive, backgroundColor: .gray, textColor: isWorkoutActive ? .gray : .white))
+                
+                Button("Stop Workout") {
+                    viewModel.stopHeartRateSimulation()
+                    statusMessage = "Workout stopped."
+                    audioPlayerManager.stop()
+                    isWorkoutActive = false
+                    isAudioPlaying = false
+                }
+                .disabled(!viewModel.isWorkoutActive)
+                .buttonStyle(PrimaryButtonStyle(isDisabled: !viewModel.isWorkoutActive, backgroundColor: .red, textColor: !isWorkoutActive ? .red : .white))
+                
+                Button("Play") {
+                    audioPlayerManager.play()
+                    statusMessage = "Playing audio."
+                    isAudioPlaying = true
+                }
+                .disabled(audioPlayerManager.isPlaying)
+                .buttonStyle(PrimaryButtonStyle(isDisabled: viewModel.isWorkoutActive, backgroundColor: .gray, textColor: isAudioPlaying ? .gray : .white))
+                
+                Button("Stop") {
+                    audioPlayerManager.stop()
+                    statusMessage = "Audio stopped."
+                    isAudioPlaying = false
+                }
+                .disabled(!audioPlayerManager.isPlaying)
+                .buttonStyle(PrimaryButtonStyle(isDisabled: !isAudioPlaying, backgroundColor: .red, textColor: isAudioPlaying ? .white : .red))
+                
+                Button("Restart Workout") {
+                    viewModel.restartWorkout()
+                    statusMessage = "Workout restarted."
+                    isWorkoutActive = false
+                    isAudioPlaying = false
+                }
+                .buttonStyle(PrimaryButtonStyle(isDisabled: false, backgroundColor: .blue, textColor: .white))
             }
-            .disabled(viewModel.isWorkoutActive)
-            .buttonStyle(PrimaryButtonStyle(isDisabled: viewModel.isWorkoutActive))
-
-            Button("Stop Workout") {
-                viewModel.stopHeartRateSimulation()
-                statusMessage = "Workout stopped."
-                audioPlayerManager.stop()  // Stop playing when workout stops
+            .onChange(of: viewModel.heartRate){ newRate in
+                audioPlayerManager.adjustPlaybackRate(basedOnHeartRate: newRate)
             }
-            .disabled(!viewModel.isWorkoutActive)
-            .buttonStyle(PrimaryButtonStyle(isDisabled: !viewModel.isWorkoutActive))
-
-            Button("Play") {
-                audioPlayerManager.play()
-                statusMessage = "Playing audio."
-            }
-            .disabled(audioPlayerManager.isPlaying)
-            .buttonStyle(PrimaryButtonStyle(isDisabled: audioPlayerManager.isPlaying))
-
-            Button("Stop") {
-                audioPlayerManager.stop()
-                statusMessage = "Audio stopped."
-            }
-            .disabled(!audioPlayerManager.isPlaying)
-            .buttonStyle(PrimaryButtonStyle(isDisabled: !audioPlayerManager.isPlaying))
-        }
-        .onChange(of: viewModel.heartRate) { newRate in
-            audioPlayerManager.adjustPlaybackRate(basedOnHeartRate: newRate)
         }
     }
 }
 
 struct PrimaryButtonStyle: ButtonStyle {
     var isDisabled: Bool
+    var backgroundColor: Color
+    var textColor: Color
     
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
-            .foregroundColor(isDisabled ? .gray : .blue)
+            .foregroundColor(isDisabled ? backgroundColor : textColor)
             .padding()
-            .background(isDisabled ? Color.secondary : Color.blue)
+            .background(backgroundColor)
             .cornerRadius(8)
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
     }
--- HeartBeats38/HeartRateViewModel.swift
+++ HeartBeats38/HeartRateViewModel.swift
@@ -11,8 +11,8 @@ class HeartRateViewModel: ObservableObject {
         isWorkoutActive = true
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
             guard let self = self else { return }
-            // Simulate heart rate increase or decrease
-            let fluctuation = Double.random(in: -5...15) // Adjust range based on desired simulation detail
+            // Simulate heart rate increase
+            let fluctuation = Double.random(in: -5...15)  // Random fluctuation to simulate a varying heart rate
             let newRate = self.heartRate + fluctuation
             // Keep the simulated heart rate within a typical workout range
             self.heartRate = min(max(newRate, 100), 180)
@@ -24,5 +24,11 @@ class HeartRateViewModel: ObservableObject {
         isWorkoutActive = false
         timer?.invalidate()
         timer = nil
+    }
+
+    // Restart the workout
+    func restartWorkout() {
+        stopHeartRateSimulation()  // Stop any existing workout simulation
+        heartRate = 60.0  // Reset heart rate back to starting value
     }
 }
